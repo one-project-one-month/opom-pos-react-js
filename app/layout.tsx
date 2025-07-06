8a151ebecc48114c095c91a2bf33d0b7c27747a1
eimayzon <eimayzon@eimayzons-MacBook-Air.local>
2025-07-06 18:51:43 +0700

    Finish UI for auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <Providers>
           {/* <Layout> */}
-            {children}
+          {children}
           {/* </Layout> */}
         </Providers>
       </body>
