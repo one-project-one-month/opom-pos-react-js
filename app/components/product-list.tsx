8900aaee55b8d4dbfacc8e7560ca8b020f7c98b7
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-10 11:22:11 +0630

    Add Loading component and integrate it into ProductList for better loading state handling; update package.json and package-lock.json to include ldrs dependency

--- app/components/product-list.tsx
+++ app/components/product-list.tsx
@@ -5,12 +5,29 @@ import { useDispatch } from 'react-redux'
 import { addOrder } from '../store/slices/orderSummarySlice'
 import { Product } from '../type/product'
 import { useFetchProducts } from '../hooks/useFetchProduct'
+import Loading from '../(root)/(staff)/(main)/loading'
 
 const ProductList = () => {
 
   const {error, isLoading, data} = useFetchProducts<Product[]>();
 
   const dispatch = useDispatch();
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <Loading/>
+      </div>
+    )
+  }
+
+  if( error) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <div className="text-red-500">Error: {error.message}</div>
+      </div>
+    )
+  }
 
   return (
     <div className="h-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[17px] lg:gap-[20px] overflow-y-auto no-scrollbar">
