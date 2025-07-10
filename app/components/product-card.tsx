e36f1a8f57ab406246c13c88bddf29c2e7dc496a
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-10 11:44:56 +0630

    Enhance ProductCard component with border decorations for improved visual appeal

--- app/components/product-card.tsx
+++ app/components/product-card.tsx
@@ -15,8 +15,21 @@ export const ProductCard = ({
 }: ProductCardProps) => {
   return (
     <div
-      className="w-full h-full bg-[#F6F6F6] rounded-[12px] flex justify-center items-center shadow-sm cursor-pointer hover:transform hover:scale-[1.02] transition-all duration-200 ease-in-out"
+      className="w-full h-full flex relative justify-center items-center shadow-[0px_5px_15px_0px_#0000000D] cursor-pointer hover:transform hover:scale-[1.02] transition-all duration-200 ease-in-out"
       onClick={ordersClick}>
+      
+      {/* Top-left corner */}
+      <div className="absolute top-0 left-0 w-[10px] h-[10px] border-t-[0.5px] border-l-[0.5px] border-[#FB9E3A]"></div>
+      
+      {/* Top-right corner */}
+      <div className="absolute top-0 right-0 w-[10px] h-[10px] border-t-[0.5px] border-r-[0.5px] border-[#FB9E3A]"></div>
+      
+      {/* Bottom-left corner */}
+      <div className="absolute bottom-0 left-0 w-[10px] h-[10px] border-b-[0.5px] border-l-[0.5px] border-[#FB9E3A]"></div>
+      
+      {/* Bottom-right corner */}
+      <div className="absolute bottom-0 right-0 w-[10px] h-[10px] border-b-[0.5px] border-r-[0.5px] border-[#FB9E3A]"></div>
+      
       <div className="w-[110px]">
         <div className="space-y-[15px]">
           <div className="w-[110px] h-[110px] relative overflow-hidden flex items-center justify-center">
